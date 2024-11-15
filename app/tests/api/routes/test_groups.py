8d7e4ce0479fd9bc5e620974f5a8425e764c5a3d
keat <kavano.aresia@gmail.com>
2024-11-15 20:20:47 +0100

    added backend tests

---
+++ app/tests/api/routes/test_groups.py
@@ -0,0 +1,29 @@
+from fastapi.testclient import TestClient
+from sqlmodel import Session
+import sqlalchemy as sa
+from app.models import GroupType
+from app.core.config import settings
+
+def test_create_group(client: TestClient) -> None:
+    data = {"name": "TestName", "description": "TestDescription", "topic": "TestTopic",  
+            "type": GroupType.private, "created_at": "555", "owner_id": "1"}
+    response = client.post("/groups/create-group", json=data)
+    assert response.status_code == 200
+    content = response.json()
+    assert "id" in content
+
+def test_get_your_groups(client: TestClient) -> None:
+    response = client.get("/groups/get-your-groups")
+    assert response.status_code == 200
+    content = response.json()[0]
+    assert "id" in content
+    assert "name" in content
+    assert "description" in content
+    assert "topic" in content
+    assert "type" in content
+    
+def test_get_groups_num(client: TestClient) -> None:
+    response = client.get("/groups/get-groups-num")
+    assert response.status_code == 200
+    content = response.json()
+    assert isinstance(content,int)
