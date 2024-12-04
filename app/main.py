3de84f056aa38c53a185853306626fce05b204af
samuele.romano <okko.sr@gmail.com>
2024-12-04 20:25:54 +0100

    Api response format and error handling

--- app/main.py
+++ app/main.py
@@ -1,12 +1,16 @@
 import sentry_sdk
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, status
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.main import api_router
 from app.core.config import settings
+from fastapi.encoders import jsonable_encoder
+from fastapi.responses import JSONResponse
 
 if settings.SENTRY_DSN and settings.ENVIRONMENT != "local":
     sentry_sdk.init(dsn=str(settings.SENTRY_DSN), enable_tracing=True)
+
+
 
 is_production = settings.ENVIRONMENT == "production"
 app = FastAPI(
@@ -24,3 +28,7 @@ app.add_middleware(
     allow_headers=["*"],
 )
 app.include_router(api_router)
+
+@app.exception_handler(status.HTTP_500_INTERNAL_SERVER_ERROR)
+async def internal_exception_handler(request: Request, exc: Exception):
+  return JSONResponse(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, content=jsonable_encoder({"message": "Internal Server Error"}))
