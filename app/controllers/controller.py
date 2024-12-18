8f9cafe284b73a2fc6ccc9b5ec8a4028d3729372
hamidafghan <i@hamidafghan.me>
2024-12-18 22:31:03 +0100

    Add the session to the controller

--- app/controllers/controller.py
+++ app/controllers/controller.py
@@ -1,18 +1,15 @@
 from typing import Any, Generic, TypeVar
 from uuid import UUID
 
-from fastapi import status
-from fastapi_pagination import Page, Params
-from fastapi_pagination.ext.sqlmodel import paginate
+from fastapi import HTTPException
 from pydantic import BaseModel
+from sqlalchemy import exc
 from sqlmodel import Session, SQLModel, func, select
-from sqlmodel.sql.expression import Select
-from app.core.db import get_session
+from sqlmodel.sql.expression import SelectOfScalar
 
 ModelType = TypeVar("ModelType", bound=SQLModel)
 CreateSchemaType = TypeVar("CreateSchemaType", bound=BaseModel)
 UpdateSchemaType = TypeVar("UpdateSchemaType", bound=BaseModel)
-SchemaType = TypeVar("SchemaType", bound=BaseModel)
 T = TypeVar("T", bound=SQLModel)
 
 
@@ -21,138 +18,114 @@ class Controller(Generic[ModelType, CreateSchemaType, UpdateSchemaType]):
         """
         CRUD object with default methods to Create, Read, Update, Delete (CRUD).
         **Parameters**
-        * `model`: A SQLModel model class
-        * `db_session`: A SQLAlchemy Session instance
         """
         self.model = model
-        self.db_session = next(get_session())
 
-    def get_db(self) -> Session:
-        return next(get_session())
-
-    def get(self, *, id: UUID | str) -> ModelType | None:
-        query = select(self.model).where(self.model.id == id)
-        response = self.db_session.exec(query)
+    def get(self, *, id: UUID | str, session: Session) -> ModelType | None:
+        query = select(self.model).where(self.model.id == id)  # type: ignore
+        response = session.exec(query)
         return response.one_or_none()
 
-    def get_by_ids(self, *, list_ids: list[UUID | str]) -> list[ModelType]:
-        response = self.db_session.exec(
-            select(self.model).where(self.model.id.in_(list_ids))
+    def get_by_ids(
+        self, *, list_ids: list[UUID | str], session: Session
+    ) -> list[ModelType]:
+        response = session.exec(
+            select(self.model).where(self.model.id.in_(list_ids))  # type: ignore
         )
-        return response.scalars().all()
+        return response.all()  # type: ignore
 
-    def get_count(self) -> int:
-        response = self.db_session.exec(
+    def get_count(self, session: Session) -> int:
+        response = session.exec(
             select(func.count()).select_from(select(self.model).subquery())
         )
         return response.one()
 
     def get_multi(
-            self,
-            *,
-            skip: int = 0,
-            limit: int = 100,
-            query: T | Select[T] | None = None,
+        self,
+        *,
+        skip: int = 0,
+        limit: int = 100,
+        query: SelectOfScalar[T] | None = None,
+        session: Session,
     ) -> list[ModelType]:
-        if query is None:
-            query = select(self.model).offset(skip).limit(limit).order_by(self.model.id)
-        response = self.db_session.exec(query)
-        return response.all()
-
-    def get_multi_paginated(
-            self,
-            *,
-            params: Params | None = Params(),
-            query: T | Select[T] | None = None,
-    ) -> Page[ModelType]:
-        if query is None:
-            query = select(self.model)
-        return paginate(self.db_session, query, params)
-
-    def get_multi_paginated_ordered(
-            self,
-            *,
-            params: Params | None = Params(),
-            order_by: str | None = None,
-            order: str = "asc",
-            query: T | Select[T] | None = None,
-    ) -> Page[ModelType]:
-        columns = self.model.__table__.columns
-        order_by_column = columns.get(order_by, columns["id"])
-
-        if query is None:
-            query = select(self.model).order_by(
-                order_by_column.asc() if order == "asc" else order_by_column.desc()
-            )
-
-        return paginate(self.db_session, query, params)
+        statement = (
+            select(self.model).offset(skip).limit(limit) if query is None else query
+        )
+        response = session.exec(statement)
+        return response.all()  # type: ignore
 
     def get_multi_ordered(
-            self,
-            *,
-            skip: int = 0,
-            limit: int = 100,
-            order_by: str | None = None,
-            order: str = "asc",
+        self,
+        *,
+        skip: int = 0,
+        limit: int = 100,
+        order_by: str | None = None,
+        order: str = "asc",
+        session: Session,
     ) -> list[ModelType]:
-        columns = self.model.__table__.columns
-        order_by_column = columns.get(order_by, columns["id"])
+        columns = self.model.__table__.columns  # type: ignore
+        order_by_column = columns.get(order_by, columns["id"])  # type: ignore
 
         query = (
             select(self.model)
             .offset(skip)
             .limit(limit)
             .order_by(
-                order_by_column.asc() if order == "asc" else order_by_column.desc()
+                order_by_column.asc() if order == "asc" else order_by_column.desc()  # type: ignore
             )
         )
 
-        response = self.db_session.exec(query)
-        return response.scalars().all()
+        response = session.exec(query)
+        return response.all()  # type: ignore
+
+    def get_all(self, *, session: Session) -> list[ModelType]:
+        response = session.exec(select(self.model))
+        return response.all()  # type: ignore
 
     def create(
-            self,
-            *,
-            obj_in: CreateSchemaType | ModelType,
-            created_by_id: UUID | str | None = None,
+        self,
+        *,
+        obj_in: CreateSchemaType | ModelType,
+        update: dict[str, Any] | None = None,
+        session: Session,
     ) -> ModelType:
-        db_obj = self.model.model_validate(obj_in)  # type: ignore
-
-        if created_by_id:
-            db_obj.created_by_id = created_by_id
+        db_obj = self.model.model_validate(obj_in, update=update)
 
         try:
-            self.db_session.add(db_obj)
-            self.db_session.commit()
+            session.add(db_obj)
+            session.commit()
         except exc.IntegrityError:
-            self.db_session.rollback()
+            session.rollback()
             raise HTTPException(
-                status_code=status.HTTP_409_CONFLICT,
+                status_code=409,
                 detail="Resource already exists",
             )
-        self.db_session.refresh(db_obj)
+        session.refresh(db_obj)
         return db_obj
 
     def update(
-            self,
-            *,
-            obj_current: ModelType,
-            obj_new: UpdateSchemaType | dict[str, Any] | ModelType,
+        self,
+        *,
+        obj_current: ModelType,
+        obj_new: UpdateSchemaType | dict[str, Any] | ModelType,
+        session: Session,
     ) -> ModelType:
         update_data = (
-            obj_new if isinstance(obj_new, dict) else obj_new.dict(exclude_unset=True)
+            obj_new
+            if isinstance(obj_new, dict)
+            else obj_new.model_dump(exclude_unset=True)
         )
         for field in update_data:
             setattr(obj_current, field, update_data[field])
 
-        self.db_session.add(obj_current)
-        self.db_session.commit()
-        self.db_session.refresh(obj_current)
+        session.add(obj_current)
+        session.commit()
+        session.refresh(obj_current)
         return obj_current
 
-    def remove(self, *, id: UUID | str) -> ModelType:
-        response = self.db_session.exec(select(self.model).where(self.model.id == id))
-        obj = response.one_or_none()
-        self.db_session.delete(obj)
-        self.db_session.commit()
+    def remove(self, *, id: UUID | str, session: Session) -> ModelType:
+        response = session.exec(select(self.model).where(self.model.id == id))  # type: ignore
+        obj = response.one()
+        session.delete(obj)
+        session.commit()
         return obj
