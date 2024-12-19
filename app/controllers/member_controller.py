ba79a242831b26e7e9a391c674a27163eb6ff15c
keat <kavano.aresia@gmail.com>
2024-12-19 23:47:09 +0100

    added session

--- app/controllers/member_controller.py
+++ app/controllers/member_controller.py
@@ -9,6 +9,7 @@ from app.resources.user_resource import UsersMemberPublic
 from app.models.group_model import GroupTypes
 from app.models.member_model import MemberTypes
 from sqlmodel import select
+from app.api.deps import SessionDep
 
 
 class MemberController(Controller[MemberModel, MemberModel, MemberModel]):
@@ -37,14 +38,14 @@ class MemberController(Controller[MemberModel, MemberModel, MemberModel]):
             raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Something went wrong.")
         
         
-    def get_members(self, user_id: uuid.UUID, group_id: uuid.UUID) -> UsersMemberPublic:
-        group = group_controller.get(id=group_id)
+    def get_members(self, user_id: uuid.UUID, group_id: uuid.UUID, session: SessionDep) -> UsersMemberPublic:
+        group = group_controller.get(id=group_id, session=session)
 
         if group.type != GroupTypes.public_open:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Unable to join.")
 
         query = select(MemberModel).where(MemberModel.user_id == user_id).where(MemberModel.group_id == group_id).where(MemberModel.deleted_at == None)
-        if len(self.get_multi(query=query)) == 0:
+        if len(self.get_multi(query=query, session=session)) == 0:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Not a member of group.")
         
         try:
@@ -54,7 +55,7 @@ class MemberController(Controller[MemberModel, MemberModel, MemberModel]):
                 .where(MemberModel.group_id == group_id)
             )
             
-            members_public = self.get_multi(query=query)
+            members_public = self.get_multi(query=query, session=session)
             
             return UsersMemberPublic(data=members_public, count=group.member_count)
         
