8b858875463874e3844032f141ad5dc880d0a202
keat <kavano.aresia@gmail.com>
2024-12-19 14:57:03 +0100

    added get members api

--- app/api/routes/member_routes.py
+++ app/api/routes/member_routes.py
@@ -4,13 +4,20 @@ from fastapi import APIRouter
 
 from app.api.deps import CurrentUser
 from app.controllers.member_controller import member_controller
-from app.resources.member_resource import MemberPublic
+from app.resources.member_resource import MemberPublic, MembersPublic
 
 router = APIRouter()
 
 @router.post("/groups/{_id}/members", response_model=MemberPublic)
 def index(_id: uuid.UUID, current_user: CurrentUser) -> MemberPublic:
     return member_controller.create_member(
+        user_id=current_user.id,
+        group_id=_id
+    )
+    
+@router.get("/groups/{_id}/members", response_model=MembersPublic)
+def index(_id: uuid.UUID, current_user: CurrentUser) -> MembersPublic:
+    return member_controller.get_members(
         user_id=current_user.id,
         group_id=_id
     )
